ff622cc7dcf74bbdb3e2afba7e725769d572a2a9
muyeyifeng <muyeyifeng@163.com>
2023-05-09 21:05:37 +0800

    增加安装包

--- other_func/requires.py
+++ other_func/requires.py
@@ -1,21 +1,19 @@
-requirements = ["yaml", "requests", "beautifulsoup4"]
-
-
-def check_requirement(package):
+def check_requirement(require, package):
     """
+    :param require:
     :param package:
     :return:
     """
     try:
-        exec("import {0}".format(package))
+        exec("import {0}".format(require))
     except ModuleNotFoundError:
         inquiry = input("This script requires {0}. Do you want to install {0}? [y/n]".format(package))
         while (inquiry != "y") and (inquiry != "n"):
             inquiry = input("This script requires {0}. Do you want to install {0}? [y/n]".format(package))
         if inquiry == "y":
             import os
-            print("Execute commands: pip install {0}".format(package))
-            os.system("pip install {0}".format(package))
+            print("Execute commands: pip3 install {0}".format(package))
+            os.system("pip3 install {0}".format(package))
         else:
             print("{0} is missing, so the program exits!".format(package))
             exit(-1)
@@ -25,5 +23,6 @@ def install_requires():
     """
     :return:
     """
-    for requirement in requirements:
-        check_requirement(requirement)
+    require_dict = {"yaml": "pyyaml", "requests": "requests", "beautifulsoup4": "beautifulsoup4"}
+    for [requirement, package] in require_dict:
+        check_requirement(requirement, package)
